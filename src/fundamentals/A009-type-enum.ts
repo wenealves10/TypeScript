10111b72d02567dc03b6ccbf45261a9ba49c2534
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-05-11 19:53:15 -0300

    Enums in the TypeScript 💙

---
+++ src/fundamentals/A009-type-enum.ts
@@ -0,0 +1,20 @@
+enum Steps {
+  STEPONE,
+  STEPTWO,
+}
+
+enum Steps {
+  ThreeSteps = 'Three 3',
+  FourSteps = 100,
+  FiveSteps,
+}
+
+function stepsEnum(title: Steps): void {
+  console.log(Steps[title]);
+}
+
+stepsEnum(0);
+
+console.log(Steps);
+console.log(Steps.STEPONE);
+console.log(Steps[0]);

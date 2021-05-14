2b2dd1cddd0ae79251744a6bd49b35cfb480c349
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-05-14 09:46:52 -0300

    webpack added in the exercice 😂

---
+++ webpack.config.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/fundamentals/A018-webpack/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: { configFile: 'tsconfig.frontend.json' },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'frontend', 'assets', 'js'),
+  },
+  devtool: 'source-map',
+};

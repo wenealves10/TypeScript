a0ae8be91687a4895e81b5d510c8ee572da0686a
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-07-16 13:41:04 -0300

    Learning about Tsconfig AllowJs with TypeScript 💯

---
+++ src/TypeScript - POO/A060-Imports-Exports-Js-TypeScript/index.ts
@@ -0,0 +1,4 @@
+import { sumFnc, multFnc } from './module';
+console.log(sumFnc(1, 2));
+const mult = multFnc(2, 5) as number;
+console.log(mult);
---
+++ src/TypeScript - POO/A060-Imports-Exports-Js-TypeScript/module.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function sumFnc(x, y) {
+  return x + y;
+}
+
+export function multFnc(y, z) {
+  return y * z;
+}

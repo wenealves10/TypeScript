e44d6b89c94f88e08e2b8d9b14d27119a6c945e2
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-07-19 08:40:14 -0300

    Index of OCP 💯

---
+++ src/TypeScript - S.O.L.I.D/2-Open-Closed-Principle/src/index.ts
@@ -0,0 +1,28 @@
+import { NoDiscount } from './classes/discount';
+import { Order } from './classes/order';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { Messaging } from './services/messaging';
+import { Persistency } from './services/persistency';
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Shirt', 58.85));
+shoppingCart.addItem(new Product('Shoes', 50.58));
+shoppingCart.addItem(new Product('Hat', 18.75));
+shoppingCart.addItem(new Product('Pants', 85.95));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(shoppingCart.items);
+console.log(order.orderStatus);

59220646435a9d604859457c07f7c9b3c3edca8d
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-07-19 08:40:04 -0300

    Services of OCP 🔢

---
+++ src/TypeScript - S.O.L.I.D/2-Open-Closed-Principle/src/services/messaging.ts
@@ -0,0 +1,6 @@
+class Messaging {
+  sendMessage(message: string): void {
+    console.log('Mensagem foi envida:', message);
+  }
+}
+export { Messaging };
---
+++ src/TypeScript - S.O.L.I.D/2-Open-Closed-Principle/src/services/persistency.ts
@@ -0,0 +1,7 @@
+class Persistency {
+  saveOrder(): void {
+    console.log('Pedido foi Salvo com sucesso.');
+  }
+}
+
+export { Persistency };

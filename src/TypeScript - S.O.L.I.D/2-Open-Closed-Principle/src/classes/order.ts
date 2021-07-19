0ed25715d85ff5f6b75f800f766647258cb0fbca
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-07-19 08:39:31 -0300

    Class Order OCP 💯

---
+++ src/TypeScript - S.O.L.I.D/2-Open-Closed-Principle/src/classes/order.ts
@@ -0,0 +1,34 @@
+import { Messaging } from '../services/messaging';
+import { Persistency } from '../services/persistency';
+import { OrderStatus } from './interfaces/order-status';
+import { ShoppingCart } from './shopping-cart';
+
+class Order {
+  private _orderStatus: OrderStatus = 'open';
+
+  constructor(
+    private readonly cart: ShoppingCart,
+    private readonly messaging: Messaging,
+    private readonly persistency: Persistency,
+  ) {}
+
+  checkout(): void {
+    if (this.cart.isEmpty()) {
+      console.log('Seu Carrinho está vazio.');
+      return;
+    }
+
+    this._orderStatus = 'closed';
+    this.messaging.sendMessage(
+      `Seu Pedido com total R$ ${this.cart.totalWithDiscount()} foi recebido`,
+    );
+    this.persistency.saveOrder();
+    this.cart.clear();
+  }
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+}
+
+export { Order };
